b4d00c4b383c10885b530d0d6c81a8eab69b119f
Yuto-Aoki <yuuto3039@gmail.com>
2020-03-15 11:49:45 +0900

    Add move method

--- src/snakegame.py
+++ src/snakegame.py
@@ -10,10 +10,15 @@ Cols = 25
 Rows = 20
 
 class Cube():
-    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+    def __init__(self, start, x=1, y=0, color=(255,0,0)):
         self.rows = 20
         self.w = 500
         self.pos = start
-        self.dirnx = dirnx
-        self.dirny = dirny
+        self.x = x
+        self.y = y
         self.color = color
+    
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+        self.pos = (self.pos[0] + self.x, self.pos[1] + self.y)
