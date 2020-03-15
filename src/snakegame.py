a519a18f1498a7b3807434481b494daea8af2ddb
Yuto-Aoki <yuuto3039@gmail.com>
2020-03-15 11:16:31 +0900

    Add cinstructer

--- src/snakegame.py
+++ src/snakegame.py
@@ -3,8 +3,17 @@ import random
 import pygame
 import random
 
-width = 500
-height = 500
+Width = 500
+Height = 500
 
-cols = 25
-rows = 20
+Cols = 25
+Rows = 20
+
+class Cube():
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+        self.rows = 20
+        self.w = 500
+        self.pos = start
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.color = color
