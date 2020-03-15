7a1d1e9e1d1723c1406ffcf30349495755913d29
Yuto-Aoki <yuuto3039@gmail.com>
2020-03-15 12:12:50 +0900

    Add draw method each body

--- src/snakegame.py
+++ src/snakegame.py
@@ -10,15 +10,29 @@ Cols = 25
 Rows = 20
 
 class Cube():
+    """
+    Snakeの各体のオブジェクト
+    """
     def __init__(self, start, x=1, y=0, color=(255,0,0)):
         self.rows = 20
         self.w = 500
-        self.pos = start
+        self.pos = start # (x, y)
         self.x = x
         self.y = y
         self.color = color
     
     def move(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = x       # new x
+        self.y = y       # new y
         self.pos = (self.pos[0] + self.x, self.pos[1] + self.y)
+    
+    def draw(self, surface):
+        dis = self.w // self.rows
+        x, y = self.pos
+        pygame.draw.rect(surface, self.color, (x * dis + 1, y * dis + 1, dis - 2, dis - 2))
+
+if __name__ == "__main__":
+    cube = Cube((10, 10))
+    surface = pygame.Surface((10, 10))
+    surface.fill((0,0,0)) 
+    cube.draw(surface)
